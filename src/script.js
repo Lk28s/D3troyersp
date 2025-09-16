6f0a0f89a7d78195e8377198c78d37ee54752a39
Lks Modder <ffroblox547@gmail.com>
2025-09-16 02:09:29 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-
 let MostrarSenha = document.getElementById("VerSenha");
 let Senha = document.getElementById("senha");
 let imagem = document.getElementById("OlhoVer");
@@ -35,58 +34,7 @@ function adicionarSemDuplicar(array, items) {
 
 MostrarSenha.addEventListener("click", () => {
     if (Senha.type === "password") {
-        Senha.type = "text";
-        imagem.src = "visivel.png";
-    } else {
-        Senha.type = "password";
-        imagem.src = "olho.png";
-    }
-});
-
-function Atividade(Titulo, Atividade, tempo = 2500) {
-    const div = document.createElement("div");
-    div.className = "Notificacao";
-
-    const h1 = document.createElement("h1");
-    h1.textContent = Titulo;
-
-    const p = document.createElement("p");
-    p.textContent = Atividade;
-
-    const barraTempo = document.createElement("div");
-    barraTempo.className = "barra-tempo";
-  
-    div.appendChild(h1);
-    div.appendChild(p);
-    div.appendChild(barraTempo);
-
-    const article = document.getElementById("TamanhoN");
-    article.appendChild(div);
-
-    div.style.animation = "AparecerBonito 1s ease";
-
-    setTimeout(() => {
-        div.style.animation = "sumir 1s ease";
-        div.addEventListener("animationstart", () => {
-          setTimeout(() => {
-              const interval = setInterval(() => {
-                  const currentScroll = article.scrollTop;
-                  const targetScroll = article.scrollHeight;
-                  const distance = targetScroll - currentScroll;
-                  
-                  article.scrollTop += distance * 0.4;
-      
-                  if (distance < 1) {
-                      clearInterval(interval);
-                  }
-              }, 16);
-          }, 200);
-      });
-
-        div.addEventListener("animationend", () => {
-          div.remove();
-        })
-    }, tempo);
+@@ -90,461 +60,10 @@ function Atividade(Titulo, Atividade, tempo = 2500) {
 }
 document.getElementById('Enviar').addEventListener('submit', (e) => {
   e.preventDefault();
@@ -234,7 +182,7 @@ async function fetchProva(token, room, name, groups, nick) {
   }
 }
 
-  
+
 async function fetchTeste(token, room, name,groups,nick) {
   const headers = {
     'Content-Type': 'application/json',
@@ -321,6 +269,8 @@ async function fetchUserRooms(token,nick) {
 }
 
 async function fetchTasks(token, room, name,groups) {
+  Atividade('Sistema em manutenção...','embreve voltara!');
+  return;
   const headers = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
